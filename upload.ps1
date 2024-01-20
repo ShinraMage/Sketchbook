7a7b57fafdabc758b850a10398401f949504bf97
LatticeMage <LatticeMage@users.noreply.github.com>
2024-01-20 12:30:01 +0800

    upload

---
+++ upload.ps1
@@ -0,0 +1,14 @@
+cd _shared
+./upload.bat
+cd ../
+
+git config --local user.name "LatticeMage"
+git config --local user.email "LatticeMage@users.noreply.github.com"
+git remote set-url origin git@LM:LatticeMage/Sketchbook.git
+
+git submodule update --recursive --remote
+
+git pull
+git add .
+git commit -m "upload"
+git push
